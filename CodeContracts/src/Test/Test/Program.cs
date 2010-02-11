8d99c0ed2277726d9bd20f371a5faff9501d2429
Mike Chaliy <mike@chaliy.name>
2010-02-11 16:36:53 +0200

    CodeContracts: Added some example code

---
+++ CodeContracts/src/Test/Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test
+{
+	class Program
+	{		
+		static void Main(string[] args)
+		{
+			DoSomethingCool(null);
+			
+			var res = ReturnSomethingCool();
+			if (res == null)
+			{
+				throw new InvalidOperationException();
+			}
+
+
+		}
+
+		static void DoSomethingCool(string arg)
+		{
+			if (arg == null)
+			{
+				throw new ArgumentNullException("arg");
+			}
+		}
+
+		static string ReturnSomethingCool()
+		{
+			return null;
+		}
+	}
+}
