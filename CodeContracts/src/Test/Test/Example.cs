247c588aa8a8d89f7e9081cc432b70734b5fb2ac
Mike Chaliy <mike@chaliy.name>
2010-02-12 18:18:26 +0200

    CodeContracts, added some stuff

---
+++ CodeContracts/src/Test/Test/Example.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Test
+{
+	public class Example
+	{
+		public string Test(string arg)
+		{
+			Contract.Requires(arg != null);
+			Contract.Ensures(Contract.Result<string>() != null);
+
+			return "Test";
+		}
+	}
+}
--- CodeContracts/src/Test/Test/Program.cs
+++ CodeContracts/src/Test/Test/Program.cs
@@ -1,32 +1,82 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 
 namespace Test
-{
+{	
 	class Program
-	{		
-		static void Main(string[] args)
+	{
+		private class A
 		{
-			DoSomethingCool(null);
-			
-			var res = ReturnSomethingCool();
-			if (res == null)
+			private decimal _val;
+
+			public decimal Val
+			{
+				get { return _val; }
+				set
+				{
+					Contract.Assume(value > 0);
+					_val = value;
+				}
+			}
+
+			//public void Decriment()
+			//{
+			//    if (_val > 1)
+			//    {					
+			//        _val--;					
+			//    }
+			//    Contract.Assume(_val > 0);
+			//}
+
+			[ContractInvariantMethod]
+			private void ObjectInvariant()
+			{
+				Contract.Invariant(_val > 0);
+			}			
+		}
+
+		static void Main(string[] args)
+		{			
+			Contract.Requires<ArgumentException>(args.Length == 0);
+			Contract.Requires<ArgumentException>(args[0] != null);
+			Contract.Assume(args[0] != null);
+			DoSomethingCool(args[0]);
+
+			//new A {Val = 1.0m};
+		}
+
+		static void Rec(bool res)
+		{
+			Contract.Requires<ArgumentException>(res);
+		}
+
+		public static void Test(bool t)
+		{
+			if (t)
 			{
 				throw new InvalidOperationException();
 			}
-
-
 		}
 
-		static void DoSomethingCool(string arg)
+		static void DoSomethingCool(string arg)			
 		{
-			if (arg == null)
-			{
-				throw new ArgumentNullException("arg");
-			}
+			Contract.Requires(arg != null);
+
+			Console.WriteLine(arg.Length);
 		}
 
 		static string ReturnSomethingCool()
 		{
+			//var res = ReturnSomethingCool();
+			//if (res == null)
+			//{
+			//    throw new InvalidOperationException();
+			//}
+
+			//Contract.Ensures(Contract.Result<string>() != null);
+
+			Console.WriteLine("Test");			
+
 			return null;
 		}
 	}
