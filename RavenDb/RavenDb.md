c98aaaa92e7678c20fc9016075bc7f6320beb430
Mike Chaliy <mike.chaliy@axiomatics.com>
2012-09-27 17:29:02 +0300

    RavenDb talk

---
+++ RavenDb/RavenDb.md
@@ -0,0 +1,42 @@
+# Почнемо
+
+- Документо-орієнтована БД
+- Найбільше схожа на MongoDb
+- Найголовніше це те що воно .NET
+- Розробляється для девелоперів
+
+# Приклад
+- Показать пример индекса, мап/редьюс
+
+# Фішки
+    Json.NET
+    Esent (Но будет Munin)
+    ЮнітОфВорк
+    Мульті мап/ред'юс
+    Failover, Sharding, Replication
+    FullTextSearch, Suggestions, Lucene.NET
+    MultiTenancy
+    Async
+    Batches,Attaches
+    REST API
+    Nuget
+    Міграції
+    Транзакції
+    Деплоймент в IIS
+    HTTP
+    Tasks(Import/Export), Бакапы
+
+# Цікавинки
+
+- Проблеми
+    Занадо розумний
+    Жере ресурси і немає гаідлайнів
+    Коштує грощей :(
+
+http://docs.ravendb.net/
+
+Another DB RaptorDB - http://www.codeproject.com/KB/database/RaptorDB.aspx
+
+# Misc
+Installation
+_no default web.config_
---
+++ RavenDb/src/PlayWithRavenDb/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Document;
+using Raven.Client.Indexes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Connection..");
+        var db = new DocumentStore
+        {
+            Url = "http://localhost:9653/"
+        };
+        db.Initialize();
+
+
+
+
+
+
+
+        Console.WriteLine("First doc...");
+        string docId;
+        using (var session = db.OpenSession())
+        {
+            var rec = new SaleRecord();
+            session.Store(rec);
+
+            session.SaveChanges();
+
+            docId = rec.Id;
+        }
+
+        Console.WriteLine(docId);
+
+
+
+
+
+
+        Console.WriteLine("Update doc..");
+        using (var session = db.OpenSession())
+        {
+            var rec = session.Load<SaleRecord>(docId);
+            //var rec = session.Query<SaleRecord>().FirstOrDefault(x => x.Id == docId);
+            rec.Total = 12.0m;
+            rec.Date = DateTime.Now;
+
+            session.SaveChanges();
+        }
+
+
+
+
+
+        Console.WriteLine("Delete doc...");
+        using (var session = db.OpenSession())
+        {
+            var rec = session.Load<SaleRecord>(docId);
+            session.Delete(rec);
+
+            session.SaveChanges();
+        }
+
+
+
+
+
+
+
+        Console.WriteLine("Index, Map/Reduce demo...");
+        IndexCreation.CreateIndexes(typeof(Program).Assembly, db);
+
+        using (var session = db.OpenSession())
+        {
+            session.Store(new SaleRecord
+            {
+                Total = 10.0m,
+                Date = new DateTime(2012, 11, 12)
+            });
+
+            session.Store(new SaleRecord
+            {
+                Total = 13.0m,
+                Date = new DateTime(2012, 11, 13)
+            });
+
+            session.Store(new SaleRecord
+            {
+                Total = 12.0m,
+                Date = new DateTime(2012, 11, 13)
+            });
+
+            session.SaveChanges();
+        }		
+		
+		db.Dispose();		
+			
+	}		
+}	
+
+public class SaleRecord
+{
+    public string Id { get; set; }
+	public DateTime Date {get; set;}
+	public decimal Total {get; set;}
+}
+
+
+public class SaleRecords_Daily 
+	: AbstractIndexCreationTask<SaleRecord>
+{
+	// http://localhost:8080/indexes/salerecords/daily 
+	public SaleRecords_Daily()    
+	{
+		Map = docs => from doc in docs				
+			select new
+			{                      
+				Date = doc.Date,
+				Total = doc.Total
+			};       
+		Reduce = results => from result in results 
+			group result by result.Date.Date into g         
+			select new 
+			{                             
+				
+				Date = g.Key,            
+				Total = g.Sum(x => x.Total)   
+			};   
+	}
+}
